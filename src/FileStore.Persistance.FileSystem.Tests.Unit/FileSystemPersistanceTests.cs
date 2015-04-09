eb8202d5618b45123c956e119db86186de6e6ef5
mgibas <gibasmaciej@gmail.com>
2015-04-09 20:48:00 +0200

    Working FileSystemPersistance

--- src/FileStore.Persistance.FileSystem.Tests.Unit/FileSystemPersistanceTests.cs
+++ src/FileStore.Persistance.FileSystem.Tests.Unit/FileSystemPersistanceTests.cs
@@ -7,185 +7,171 @@ using Xunit;
 
 namespace FileStore.Persistance.FileSystem.Tests.Unit
 {
-  public class FileSystemPersistanceTests
-  {
-    private IFileSystem fileSystem;
-    private FileSystemPersistance persistance;
-
-    public FileSystemPersistanceTests()
+    public class FileSystemPersistanceTests
     {
-      fileSystem = A.Fake<IFileSystem>();
-      persistance = new FileSystemPersistance(fileSystem);
+        private string configuredDirectory;
+        private IFileSystem fileSystem;
+        private FileSystemPersistance persistance;
+
+        public FileSystemPersistanceTests()
+        {
+            configuredDirectory = @"C:\SomeDirectory\";
+            fileSystem = A.Fake<IFileSystem>();
+            persistance = new FileSystemPersistance(fileSystem, configuredDirectory);
+        }
+
+        [Fact]
+        public void Persist_ValidData_CreatesNewFileInProvidedDirectory()
+        {
+            persistance.Persist(Guid.NewGuid(), new StoreFile { Data = new byte[0] }, 0);
+
+            A.CallTo(() => fileSystem.File.Create(A<string>.That.Matches(n => n.StartsWith(configuredDirectory)))).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Persist_ValidData_CreatesNewFileWithNameContainingProvidedId()
+        {
+            var fileId = Guid.NewGuid();
+            var storeFile = new StoreFile { Data = new byte[0] };
+            var expectedMatch = fileId + @"[_][.0-9]*[\.][.]*";
+
+            persistance.Persist(fileId, storeFile, 0);
+
+            A.CallTo(() => fileSystem.File.Create(A<string>.That.Matches(n => Regex.IsMatch(n, expectedMatch)))).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Persist_ValidData_CreatesNewFileWithNameContainingVersion()
+        {
+            var version = 44;
+            var storeFile = new StoreFile { Data = new byte[0] };
+            var expectedMatch = @"[.]*[_]" + version + @"[\.][.]*";
+
+            persistance.Persist(Guid.NewGuid(), storeFile, version);
+
+            A.CallTo(() => fileSystem.File.Create(A<string>.That.Matches(n => Regex.IsMatch(n, expectedMatch)))).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Persist_ValidData_CreatesNewFileWithNameContainingExtension()
+        {
+            var storeFile = new StoreFile { Extension = "txt", Data = new byte[0] };
+            var expectedMatch = @"[.]*[_][.0-9]*[\.]" + storeFile.Extension;
+
+            persistance.Persist(Guid.NewGuid(), storeFile, 0);
+
+            A.CallTo(() => fileSystem.File.Create(A<string>.That.Matches(n => Regex.IsMatch(n, expectedMatch)))).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Persist_ValidData_WriteAllDataToCreatedFile()
+        {
+            var stream = A.Fake<Stream>();
+            var storeFile = new StoreFile { Data = new byte[] { 1, 2, 3, 4, 5 } };
+            A.CallTo(() => fileSystem.File.Create(A<string>._)).Returns(stream);
+
+            persistance.Persist(Guid.NewGuid(), storeFile, 0);
+
+            A.CallTo(() => stream.Write(storeFile.Data, 0, storeFile.Data.Length)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Persist_ValidData_DisposeFileStream()
+        {
+            var stream = A.Fake<Stream>(cfg => cfg.Implements(typeof(IDisposable)));
+            var storeFile = new StoreFile { Data = new byte[0] };
+            A.CallTo(() => fileSystem.File.Create(A<string>._)).Returns(stream);
+
+            persistance.Persist(Guid.NewGuid(), storeFile, 0);
+
+            A.CallTo(() => (stream as IDisposable).Dispose()).MustHaveHappened();
+        }
+
+        [Fact]
+        public void ReadFile_SearchForFileUsingStoreDirectoryFileIdAndVersion()
+        {
+            var id = Guid.NewGuid();
+            var version = 6;
+            var expectedSearchFileName = string.Format("{0}_{1}.*", id, version);
+
+            var result = persistance.ReadFile(id, version);
+
+            A.CallTo(() => fileSystem.Directory.GetFiles(configuredDirectory, expectedSearchFileName)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void ReadFile_FileDoesNotExists_ReturnsNull()
+        {
+            A.CallTo(() => fileSystem.Directory.GetFiles(A<string>._, A<string>._)).Returns(new string[0]);
+
+            var result = persistance.ReadFile(Guid.NewGuid(), 0);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void ReadFile_FileExists_ReturnsFileDataReadFromFoundFile()
+        {
+            var fileData = new byte[] { 1, 2, 3, 4 };
+            A.CallTo(() => fileSystem.Directory.GetFiles(A<string>._, A<string>._))
+                .Returns(new[] { "SomeFileToRead" });
+            A.CallTo(() => fileSystem.File.ReadAllBytes("SomeFileToRead")).Returns(fileData);
+
+            var result = persistance.ReadFile(Guid.NewGuid(), 0);
+
+            Assert.Equal(fileData, result.Data);
+        }
+
+        [Fact]
+        public void ReadFile_FileExists_ReturnsFileExtensionReadFromFoundFile()
+        {
+            A.CallTo(() => fileSystem.Directory.GetFiles(A<string>._, A<string>._))
+                .Returns(new[] { "SomeFileToRead.zip" });
+
+            var result = persistance.ReadFile(Guid.NewGuid(), 0);
+
+            Assert.Equal("zip", result.Extension);
+        }
+
+        [Fact]
+        public void FileExists_Exists_ReturnsTrue()
+        {
+            var id = Guid.NewGuid();
+            A.CallTo(() => fileSystem.Directory.GetFiles(configuredDirectory, string.Format("{0}_*.*", id)))
+                .Returns(new[] { "SomeFileToRead.zip" });
+
+            var result = persistance.FileExists(id);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void FileExists_DoesNotExist_ReturnsFalse()
+        {
+            var id = Guid.NewGuid();
+            A.CallTo(() => fileSystem.Directory.GetFiles(configuredDirectory, string.Format("{0}_*.*", id)))
+                .Returns(new string[0]);
+
+            var result = persistance.FileExists(id);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void GetFileLatestVersion_MultipleVersion_ReturnsLatestVersionOfSpecifiedFile()
+        {
+            var id = Guid.NewGuid();
+            A.CallTo(() => fileSystem.Directory.GetFiles(configuredDirectory, string.Format("{0}_*.*", id)))
+               .Returns(new[]{
+               "somefile_3.txt",
+               "somefile_6.txt",
+               "somefile_5.txt",
+               "somefile_4.txt",
+               });
+
+            var result = persistance.GetFileLatestVersion(id);
+
+            Assert.Equal(6, result);
+        }
     }
-
-    //[Fact]
-    //public void Persist_SavesChanges()
-    //{
-    //  persistance.Persist(Guid.NewGuid(), new StoreFile(), 0);
-
-    //  A.CallTo(() => context.SaveChanges()).MustHaveHappened();
-    //}
-
-    [Fact]
-    public void Persist_ValidData_CreatesNewFileWithNameContainingProvidedId()
-    {
-      var fileId = Guid.NewGuid();
-      var storeFile = new StoreFile { Data = new byte[0] };
-      var expectedMatch = fileId + @"[_][.0-9]*[\.][.]*";
-
-      persistance.Persist(fileId, storeFile, 0);
-
-      A.CallTo(() => fileSystem.File.Create(A<string>.That.Matches(n => Regex.IsMatch(n, expectedMatch)))).MustHaveHappened();
-    }
-
-    [Fact]
-    public void Persist_ValidData_CreatesNewFileWithNameContainingVersion()
-    {
-      var version = 44;
-      var storeFile = new StoreFile { Data = new byte[0] };
-      var expectedMatch = @"[.]*[_]" + version + @"[\.][.]*";
-
-      persistance.Persist(Guid.NewGuid(), storeFile, version);
-
-      A.CallTo(() => fileSystem.File.Create(A<string>.That.Matches(n => Regex.IsMatch(n, expectedMatch)))).MustHaveHappened();
-    }
-
-    [Fact]
-    public void Persist_ValidData_CreatesNewFileWithNameContainingExtension()
-    {
-      var storeFile = new StoreFile { Extension = "txt", Data = new byte[0] };
-      var expectedMatch = @"[.]*[_][.0-9]*[\.]" + storeFile.Extension;
-
-      persistance.Persist(Guid.NewGuid(), storeFile, 0);
-
-      A.CallTo(() => fileSystem.File.Create(A<string>.That.Matches(n => Regex.IsMatch(n, expectedMatch)))).MustHaveHappened();
-    }
-
-    [Fact]
-    public void Persist_ValidData_WriteAllDataToCreatedFile()
-    {
-      var stream = A.Fake<Stream>();
-      var storeFile = new StoreFile { Data = new byte[] { 1, 2, 3, 4, 5 } };
-      A.CallTo(() => fileSystem.File.Create(A<string>._)).Returns(stream);
-
-      persistance.Persist(Guid.NewGuid(), storeFile, 0);
-
-      A.CallTo(() => stream.Write(storeFile.Data, 0, storeFile.Data.Length)).MustHaveHappened();
-    }
-
-    [Fact]
-    public void Persist_ValidData_DisposeFileStream()
-    {
-      var stream = A.Fake<Stream>(cfg => cfg.Implements(typeof(IDisposable)));
-      var storeFile = new StoreFile { Data = new byte[0] };
-      A.CallTo(() => fileSystem.File.Create(A<string>._)).Returns(stream);
-
-      persistance.Persist(Guid.NewGuid(), storeFile, 0);
-
-      A.CallTo(() => (stream as IDisposable).Dispose()).MustHaveHappened();
-    }
-
-    //[Fact]
-    //public void ReadFile_MultipleFiles_ReturnsFileWithProvidedGuid()
-    //{
-    //  var currentFiles = new List<StoredFile>
-    //        {
-    //            new StoredFile{ FileId = Guid.NewGuid()},
-    //            new StoredFile{ FileId = Guid.NewGuid()},
-    //            new StoredFile{ FileId = Guid.NewGuid()},
-    //            new StoredFile{ FileId = Guid.NewGuid()},
-    //        };
-    //  var expectedFile = new StoreFile();
-    //  A.CallTo(() => context.StoredFiles).Returns(Aef.FakeDbSet(currentFiles));
-    //  A.CallTo(() => mapper.DynamicMap<StoreFile>(currentFiles[2])).Returns(expectedFile);
-
-    //  var result = persistance.ReadFile(currentFiles[2].FileId, 0);
-
-    //  Assert.Equal(expectedFile, result);
-    //}
-
-    //[Fact]
-    //public void ReadFile_MultipleVersion_ReturnsFileWithProvidedVersion()
-    //{
-    //  var id = Guid.NewGuid();
-    //  var currentFiles = new List<StoredFile>
-    //        {
-    //            new StoredFile{ FileId = id, Version=0},
-    //            new StoredFile{ FileId = id, Version=1},
-    //            new StoredFile{ FileId = id, Version=2}
-    //        };
-    //  var expectedFile = new StoreFile();
-    //  A.CallTo(() => context.StoredFiles).Returns(Aef.FakeDbSet(currentFiles));
-    //  A.CallTo(() => mapper.DynamicMap<StoreFile>(currentFiles[1])).Returns(expectedFile);
-
-    //  var result = persistance.ReadFile(id, 1);
-
-    //  Assert.Equal(expectedFile, result);
-    //}
-
-    //[Fact]
-    //public void ReadFile_NoFileWithSpecifiedVersion_ReturnsNull()
-    //{
-    //  var id = Guid.NewGuid();
-    //  var currentFiles = new List<StoredFile>
-    //        {
-    //            new StoredFile{ FileId = id, Version=0},
-    //            new StoredFile{ FileId = id, Version=1},
-    //            new StoredFile{ FileId = id, Version=2}
-    //        };
-    //  A.CallTo(() => context.StoredFiles).Returns(Aef.FakeDbSet(currentFiles));
-
-    //  var result = persistance.ReadFile(id, 3);
-
-    //  Assert.Null(result);
-    //}
-
-    //[Fact]
-    //public void FileExists_Exists_ReturnsTrue()
-    //{
-    //  var currentFiles = new List<StoredFile>
-    //        {
-    //            new StoredFile{ FileId = Guid.NewGuid()},
-    //            new StoredFile{ FileId = Guid.NewGuid()}
-    //        };
-    //  A.CallTo(() => context.StoredFiles).Returns(Aef.FakeDbSet(currentFiles));
-
-    //  var result = persistance.FileExists(currentFiles[1].FileId);
-
-    //  Assert.True(result);
-    //}
-
-    //[Fact]
-    //public void FileExists_DoesNotExist_ReturnsFalse()
-    //{
-    //  var currentFiles = new List<StoredFile>
-    //        {
-    //            new StoredFile{ FileId = Guid.NewGuid()},
-    //            new StoredFile{ FileId = Guid.NewGuid()}
-    //        };
-    //  A.CallTo(() => context.StoredFiles).Returns(Aef.FakeDbSet(currentFiles));
-
-    //  var result = persistance.FileExists(Guid.NewGuid());
-
-    //  Assert.False(result);
-    //}
-
-    //[Fact]
-    //public void GetFileLatestVersion_MultipleFilesAndVersion_ReturnsLatestVersionOfSpecifiedFile()
-    //{
-    //  var id = Guid.NewGuid();
-    //  var currentFiles = new List<StoredFile>
-    //        {
-    //            new StoredFile{ FileId = id, Version = 0},
-    //            new StoredFile{ FileId = id, Version = 1},
-    //            new StoredFile{ FileId = id, Version = 2},
-    //            new StoredFile{ FileId = Guid.NewGuid(), Version = 78},
-    //        };
-    //  A.CallTo(() => context.StoredFiles).Returns(Aef.FakeDbSet(currentFiles));
-
-    //  var result = persistance.GetFileLatestVersion(id);
-
-    //  Assert.Equal(2, result);
-    //}
-  }
 }
--- src/FileStore.Persistance.FileSystem/FileSystemPersistance.cs
+++ src/FileStore.Persistance.FileSystem/FileSystemPersistance.cs
@@ -1,39 +1,51 @@
 ﻿using System;
 using System.IO.Abstractions;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace FileStore.Persistance.FileSystem
 {
-  public class FileSystemPersistance : IPersistance
-  {
-    public FileSystemPersistance(IFileSystem fileSystem)
+    public class FileSystemPersistance : IPersistance
     {
-      this.fileSystem = fileSystem;
-    }
+        public FileSystemPersistance(IFileSystem fileSystem, string storeDirectory)
+        {
+            this.storeDirectory = storeDirectory;
+            this.fileSystem = fileSystem;
+        }
 
-    private IFileSystem fileSystem;
+        private string storeDirectory;
+        private IFileSystem fileSystem;
 
-    public void Persist(Guid id, StoreFile fileToStore, int version)
-    {
-      var fileName = id + "_" + version + "." + fileToStore.Extension;
-      using (var stream = fileSystem.File.Create(fileName))
-      {
-        stream.Write(fileToStore.Data, 0, fileToStore.Data.Length);
-      }
-    }
+        public void Persist(Guid id, StoreFile fileToStore, int version)
+        {
+            var fileName = id + "_" + version + "." + fileToStore.Extension;
+            using (var stream = fileSystem.File.Create(storeDirectory + fileName))
+            {
+                stream.Write(fileToStore.Data, 0, fileToStore.Data.Length);
+            }
+        }
 
-    public StoreFile ReadFile(Guid id, int version)
-    {
-      throw new System.NotImplementedException();
-    }
+        public StoreFile ReadFile(Guid id, int version)
+        {
+            var avaibleFiles = fileSystem.Directory.GetFiles(storeDirectory, string.Format("{0}_{1}.*", id, version));
+            if (!avaibleFiles.Any()) return null;
 
-    public bool FileExists(Guid guid)
-    {
-      throw new System.NotImplementedException();
-    }
+            var data = fileSystem.File.ReadAllBytes(avaibleFiles.First());
+            return new StoreFile { Data = data, Extension = avaibleFiles.First().Split('.').Last() };
+        }
 
-    public int GetFileLatestVersion(Guid guid)
-    {
-      throw new System.NotImplementedException();
+        public bool FileExists(Guid id)
+        {
+            return fileSystem.Directory.GetFiles(storeDirectory, string.Format("{0}_*.*", id))
+                .Any();
+        }
+
+        public int GetFileLatestVersion(Guid id)
+        {
+            var avaibleFiles = fileSystem.Directory.GetFiles(storeDirectory, string.Format("{0}_*.*", id));
+            var latestFileName = avaibleFiles.OrderByDescending(f => f).First();
+            var version = Regex.Match(latestFileName, @"[.]*_([0-9]*)\.[.]*").Groups[1].Value;
+            return int.Parse(version);
+        }
     }
-  }
 }
